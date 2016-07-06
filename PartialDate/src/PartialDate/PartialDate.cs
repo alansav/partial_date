7269229c988f35dd12b9162d0ffddadf0ac25685
Alan Savage <alansavage.dev@gmail.com>
2016-07-06 21:24:46 +0100

    Initial Commit

---
+++ PartialDate/src/PartialDate/PartialDate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Savage
+{
+    public class PartialDate
+    {
+        public readonly int? Year;
+        public readonly int? Month;
+        public readonly int? Day;
+
+        public PartialDate(int? year, int? month, int? day)
+        {
+            if (!year.HasValue && !month.HasValue && !day.HasValue)
+            {
+                throw new ArgumentException("At least one of the elements must have a value");
+            }
+
+            if (!YearIsValid(year))
+            {
+                throw new ArgumentException(message: $"Year value must be a value between 1 and 9999");
+            }
+
+            if (!MonthIsValid(month))
+            {
+                throw new ArgumentException(message: $"Month value must be a value between 1 and 12.");
+            }
+
+            if (!DayIsValid(year, month, day))
+            {
+                throw new ArgumentException(message: $"Month cannot have {day} days.");
+            }
+
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        private bool YearIsValid(int? year)
+        {
+            return !year.HasValue || year >= 1 && year <= 9999;
+        }
+
+        private bool MonthIsValid(int? month)
+        {
+            return !month.HasValue || month >= 1 && month <= 12;
+        }
+
+        private bool DayIsValid(int? year, int? month, int? day)
+        {
+            int maxDaysInMonth = DateTime.DaysInMonth(year ?? 2000, month ?? 1);
+            return !day.HasValue || day >= 1 && day <= maxDaysInMonth;
+        }
+
+        public bool IsCompleteDate => Year.HasValue && Month.HasValue && Day.HasValue;
+
+        public DateTime ToDateTime()
+        {
+            if (!Year.HasValue || !Month.HasValue || !Day.HasValue)
+            {
+                throw new InvalidOperationException("Cannot convert To DateTime object when elements are null.");
+            }
+
+            return new DateTime(Year.Value, Month.Value, Day.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            PartialDate other = (PartialDate)obj;
+            return Year == other.Year && Month == other.Month && Day == other.Day;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Year.GetHashCode();
+                hashCode = (hashCode * 397) ^ Month.GetHashCode();
+                hashCode = (hashCode * 397) ^ Day.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ PartialDate/test/UnitTests/PartialDateTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using Xunit;
+
+namespace Savage
+{
+    public class PartialDateTests
+    {
+        [Fact]
+        public void InitialisesWithAllElements()
+        {
+            var sut = new PartialDate(2016, 3, 15);
+
+            Assert.Equal(2016, sut.Year);
+            Assert.Equal(3, sut.Month);
+            Assert.Equal(15, sut.Day);
+        }
+
+        [Fact]
+        public void ToDateTimeReturnsCorrectDateTime()
+        {
+            var partialDate = new PartialDate(2016, 3, 15);
+
+            var dateTime = partialDate.ToDateTime();
+
+            Assert.Equal(partialDate.Year, dateTime.Year);
+            Assert.Equal(partialDate.Month, dateTime.Month);
+            Assert.Equal(partialDate.Day, dateTime.Day);
+        }
+
+        [Theory]
+        [InlineData(2016, 3, 32)]
+        [InlineData(2016, 13, 31)]
+        [InlineData(10000, 3, 15)]
+        [InlineData(2016, 3, 0)]
+        [InlineData(2016, 0, 15)]
+        [InlineData(0, 3, 15)]
+        [InlineData(null, 2, 30)]
+        [InlineData(2001, 2, 29)]
+        [InlineData(null, null, null)]
+        public void ThrowsExceptionWhenInitialisedWithInvalidValues(int? year, int? month, int? day)
+        {
+            Assert.Throws<ArgumentException>(() => new PartialDate(year, month, day));
+        }
+
+        [Fact]
+        public void InitialisesCorrectlyForPotentialLeapYear()
+        {
+            var sut = new PartialDate(null, 2, 29);
+            Assert.Equal(null, sut.Year);
+            Assert.Equal(2, sut.Month);
+            Assert.Equal(29, sut.Day);
+        }
+
+        [Fact]
+        public void EqualsOperatorReturnsTrueWhenAllElementsMatch()
+        {
+            var a = new PartialDate(2016, 3, 15);
+            var b = new PartialDate(2016, 3, 15);
+
+            Assert.Equal(a, b);
+        }
+
+        [Theory]
+        [InlineData(2000, 3, 15)]
+        [InlineData(2016, 4, 15)]
+        [InlineData(2016, 3, 16)]
+        public void EqualsOperatorReturnsFalseWhenDifferent(int? year, int? month, int? day)
+        {
+            var a = new PartialDate(2016, 3, 15);
+            var b = new PartialDate(year, month, day);
+
+            Assert.NotEqual(a, b);
+        }
+
+        [Fact]
+        public void EqualsOperatorReturnsFalseWhenNullArgumentProvided()
+        {
+            var a = new PartialDate(2016, 3, 15);
+
+            Assert.False(a.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsOperatorReturnsFalseWhenArgumentIsDifferentType()
+        {
+            var a = new PartialDate(2016, 3, 15);
+            var b = new DateTime(2016, 3, 15);
+
+            Assert.False(a.Equals(b));
+        }
+
+        [Fact]
+        public void EqualsOperatorReturnsTrueWhenNotAllArgumentsArePresent()
+        {
+            var a = new PartialDate(2016, null, null);
+            var b = new PartialDate(2016, null, null);
+
+            Assert.True(a.Equals(b));
+        }
+
+        [Theory]
+        [InlineData(null, 3, 15)]
+        [InlineData(2016, null, 15)]
+        [InlineData(2016, 3, null)]
+        public void IsCompleteDateReturnsFalse(int? year, int? month, int? day)
+        {
+            var sut = new PartialDate(year, month, day);
+            Assert.False(sut.IsCompleteDate);
+        }
+
+        [Fact]
+        public void IsCompleteReturnsTrueWhenComplete()
+        {
+            var sut = new PartialDate(2016, 3, 15);
+            Assert.True(sut.IsCompleteDate);
+        }
+
+        [Fact]
+        public void ThrowsExceptionCallingToDateTimeOnPartialDate()
+        {
+            var sut = new PartialDate(2016, null, 15);
+            Assert.Throws<InvalidOperationException>(() => sut.ToDateTime());
+        }
+
+        [Fact]
+        public void GetHashCodeReturnsSameValueWhenDatesMatch()
+        {
+            var a = new PartialDate(2016, 3, 15);
+            var b = new PartialDate(2016, 3, 15);
+
+            Assert.Equal(a.GetHashCode(), b.GetHashCode());
+        }
+
+        [Theory]
+        [InlineData(2015, 3, 15)]
+        [InlineData(2015, 4, 15)]
+        [InlineData(2016, 3, 16)]
+        public void GetHashCodeReturnsDifferentValues(int? year, int? month, int? day)
+        {
+            var a = new PartialDate(2016, 3, 15);
+            var b = new PartialDate(year, month, day);
+
+            Assert.NotEqual(a.GetHashCode(), b.GetHashCode());
+        }
+    }
+}
